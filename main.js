b38844684e8169be875dd2b06ab689e5b3d73bf3
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-07-05 10:16:23 -0500

    values now random

--- main.js
+++ main.js
@@ -7,10 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
         // friendNum.push(getRandomInt())
         var h3 = document.createElement('h3')
         var texty = h3.append('Clue  ' + [i])
-        h3.id = counter += 1
+        h3.id = ((counter += 1)+ getRandomInt())
         document.body.appendChild(h3)
         h3.addEventListener('click', function () {
-            alert(Friends[this.id%5] + ' did it in the ' + Locations[this.id%10] + ' with a ' + Weapons[this.id%20]+ '.')
+            alert(Friends[this.id %5] + ' did it in the ' + Locations[this.id%10] + ' with a ' + Weapons[this.id%20]+ '.')
         })
     }
 })
+function getRandomInt(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(101);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
